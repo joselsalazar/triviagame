c4d58bd7ab47f398f5228e1c316e63425c6bb48b
Jose Salazar <jose.salazar.home@gmail.com>
2017-05-06 18:05:22 -0700

    Added Timer Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,21 +15,37 @@ var series = [
 		question: "On average, how much mucus does the human body produce?",
 		answer: ["500 Milliliters", "2 Liters", "1 Liter", "250 Milliliters"],
 		class: [1,2,3,4],
-		correct: 2	
+		correct: 1	
 	}
 ];
 
 var score = 0;
+var wrongScore = 0;
 var e = 0;
+var timeLeft = 30;
 
 $('.click-start').click(function() {
 	$('.click-start').hide();
 	game();
 });
 
+// Game Function
 function game() {
-	seriesLoop();
+	// Show Timer
+	thirtySeconds(); // Removes Delay
+	var timer = setInterval(thirtySeconds, 1000);
 
+	function thirtySeconds() {
+		if (timeLeft > 0) {
+			$('.timer').html(timeLeft + " seconds remaining!");
+			timeLeft--;
+		} else {
+			clearInterval(timer);
+			wrong();
+		}
+	}
+
+	// Series Loop - Loops Through Questions and Answers
 	function seriesLoop() {
 		// Show Question
 		$('.question').html("<h1>" + series[e].question + "</h1>");
@@ -44,26 +60,53 @@ function game() {
 		}
 	};
 
-	// Generate Score
-	$('.score').html("<h2> Your Score:" + score + "</h2>");
+	seriesLoop();
+
+	// Re-Usable Game Over
+	function gameOver() {
+		$('.trivia-game, .question').empty();
+		$('.trivia-game').html('<h2>Game Over!</h2>' + 
+			"<h2> Your Score: " + score + "</h2>" +
+			'<h2>Your Wrong Answers: ' + wrongScore + '</h2>');
+	}
+
+	function wrong() {
+		console.log("Wrong!");
+		clearInterval(timer);
+		timeLeft = 30;
+		wrongScore++;
+		$('.trivia-game').empty();
+		e++;
+		if (e < series.length) {
+			game();
+		} else {
+			gameOver();
+		}
+	}
+
+	function right() {
+		console.log("Correct!");
+		clearInterval(timer);
+		timeLeft = 30;
+		score++;
+		$('.trivia-game').empty();
+		e++;
+		if (e < series.length) {
+			game();
+		} else {
+			gameOver();
+		}
+	}
 
 	// Checks Selected Button
 	$('button').click(function() {
+		// Correct Condition
 		if ($(this).hasClass('correct')) {
-			console.log("Correct!");
-			score++;
-			$('.trivia-game').empty();
-			e++;
-			game();
+			right();
+		// Wrong Condition
 		} else {
-			console.log("Wrong!");
-			$('.trivia-game').empty();
-			e++;
-			game();
+			wrong();
 		}
-
-		// Adds Score
-		$('.score').html("<h2> Your Score:" + score + "</h2>");
 	});
 
 };
